b8871d40ae4c3d79eecf0647f51f74d3260bffb9
Jurie Roux <157107427+JurieRoux@users.noreply.github.com>
2024-06-24 19:21:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import { process } from 'babel-jest';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
